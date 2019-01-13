64b93104d082ecfa69774a9856d5c6446d84e8e9
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-01-13 11:33:34 -0500

    it workssss!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import _ from 'lodash';
+
 import puppy from "./puppy.json";
 import PuppyCard from "./components/PuppyCard";
 import Wrapper from "./components/Wrapper";
@@ -7,23 +9,60 @@ import Title from "./components/Title";
 class App extends Component {
   state ={
     puppy,
-    clicked: false
+    clickedId: null,
+    userLost:false,
   };
+  didUserLose = selectedId => {
+    if(!this.state.clickedId){
+      return false;
+    }
+    if(selectedId === this.state.clickedId){
+      //here we have a lose
+      return true;
+    }
+    return false;
+  }
+  puppyClickHandler = selectedId => {
+     const currentPuppyArray = [...this.state.puppy];
+     const newArray = _.shuffle(currentPuppyArray);
 
+    // while(currentPuppyArray.length > 0){
+    //   const randVal = Math.floor(Math.random() * currentPuppyArray.length);
+    //   const randObject = currentPuppyArray.splice(randVal, 1)[0];
+    //   newArray.push(randObject);
+    // }
+    const userLoses = this.didUserLose(selectedId);
+    if(userLoses){
+      console.log('User lsoes')
+      this.setState({userLost:true})
+    }
+    else {
+
+      this.setState({puppy:newArray, clickedId: selectedId});
+    }
+    
+  }
   render() {
+    if(!this.state.userLost){
     return (
       <Wrapper>
         <Title>Puppy Clicker</Title>
-        {this.state.puppy.map(puppy => (
-          <PuppyCard>
-            id={puppy.id}
-            key={puppy.id}
-            name={puppy.name}
-            image={puppy.image}
+        {this.state.puppy.map((pup) => (
+          <PuppyCard
+          puppyClickHandler={this.puppyClickHandler}
+            id={pup.id}
+            key={pup.id}
+            name={pup.name}
+            image={pup.image}
+            >
           </PuppyCard>
         ))}
       </Wrapper>
     );
+        }
+        else {
+          return (<div>You lose</div>)
+        }
   }
 }
 
--- my-app/src/components/PuppyCard/index.js
+++ my-app/src/components/PuppyCard/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function PuppyCard(props) {
     return (
-      <div className="card">
+      <div className="card" onClick={() => props.puppyClickHandler(props.id)}>
         <div className="img-container">
           <img alt={props.name} src={props.image} />
         </div>
