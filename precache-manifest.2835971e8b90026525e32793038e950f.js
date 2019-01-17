145577d2bedf83bdca991daf3432868ddd348139
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-01-17 18:24:35 -0500

    Updates

---
+++ precache-manifest.2835971e8b90026525e32793038e950f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "111d6c5e817f397f3e7b",
+    "url": "/Clicky-Game/static/js/main.111d6c5e.chunk.js"
+  },
+  {
+    "revision": "f26954841f929a2e0b31",
+    "url": "/Clicky-Game/static/js/1.f2695484.chunk.js"
+  },
+  {
+    "revision": "111d6c5e817f397f3e7b",
+    "url": "/Clicky-Game/static/css/main.7b60c12b.chunk.css"
+  },
+  {
+    "revision": "04ee3d3a3bd3a487976d646137b354f4",
+    "url": "/Clicky-Game/index.html"
+  }
+];
